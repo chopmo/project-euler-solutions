e59e8536cf1e563baa44d665909d89289a80637f
Jacob Poulsgaard Tjoernholm <chopmo@gmail.com>
2011-01-07 22:51:58 +0100

    trim 47.rb

--- ruby/47.rb
+++ ruby/47.rb
@@ -1,22 +1,5 @@
 require 'mathn'
 
-def each_prime
-  Prime.new.each { |p| yield p }
-end
-
-def factorize(x)
-  factors = {}
-  each_prime do |p|
-    break if p > x
-    while x % p == 0
-      factors[p] ||= 0
-      factors[p] += 1
-      x = x / p
-    end
-  end
-  factors
-end
-
 def solve(num_factors)
   target_hit_count = 0
   x = 10
