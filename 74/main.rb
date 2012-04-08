070fe37a12f68362259a38f56c52c8b638aec959
Jacob Poulsgaard Tjoernholm <chopmo@gmail.com>
2012-04-08 02:12:42 +0200

    only takes a few seconds now - added caches

--- 74/main.rb
+++ 74/main.rb
@@ -1,34 +1,51 @@
 # Very slow, takes minutes
 
+$fact_cache = {}
+
 class Integer
   def fact
-    (2..self).inject(1) { |f, n| f * n }
+    $fact_cache[self] ||= (2..self).inject(1) { |f, n| f * n }
+  end
+
+  def fact_sum
+    i = self
+    res = 0
+    while i > 0
+      res += (i % 10).fact
+      i /= 10
+    end
+    res
   end
 end
 
-def fact_sum(i)
-  res = 0
-  while i > 0
-    res += (i % 10).fact
-    i /= 10
-  end
-  res
-end
-
+@chain_lengths = {}
 def num_non_repeating_terms(i)
+  x = i
   terms = [i]
-  while s = fact_sum(i) and !terms.include?(s)
+  chain_length = 1
+  while s = x.fact_sum and !terms.include?(s)
+    if cached = @chain_lengths[s]
+      # puts "found #{cached} for #{s}"
+      chain_length += cached
+      break
+    end
     terms << s
-    i = s
+    chain_length += 1
+    x = s
   end
-  terms.size
+  # puts "calc #{chain_length} for #{i}"
+  @chain_lengths[i] = chain_length
 end
 
 count = 0
 1.upto(1_000_000) do |i|
   if num_non_repeating_terms(i) == 60
     count += 1
+    # puts i
   end
+
+  # $stdout.write(".") if i % 10_000 == 0
 end
+  
 
 puts count
